a37a0e013106b49b4cb94d1434bdd382a54acdff
SharonCohenOfirTrilogy <Sharon.Cohenofir@trilogy.com>
2021-05-13 08:07:16 +0300

    Add support for transactions (buy/sell) and deposits

--- main.py
+++ main.py
@@ -37,10 +37,20 @@ def getData(url,type,nonce):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     url_blnce = 'https://www.coinspot.com.au/api/ro/my/balances'
-    url_ord =   'https://www.coinspot.com.au/api/ro/my/orders'
+    url_transactions =   'https://www.coinspot.com.au/api/ro/my/transactions'
+    url_deposits = 'https://www.coinspot.com.au/api/ro/my/deposits'
 
     NONCE_COUNTER = count(int(time() * 1000))
     n = next(NONCE_COUNTER)
+    print(getData(url_transactions,"orders",n))
+    n = next(NONCE_COUNTER)
+    print(getData(url_deposits,"deposits",n))
+
+
     for b in getData(url_blnce,"balance",n).get("balances"):
         for k in b:
             print(f"{k},{b[k].get('balance')},{b[k].get('audbalance')},{ b[k].get('rate')}")
+
+    n = next(NONCE_COUNTER)
+    x = getData(url_ord,"orders",n)
+    print(x)
