eebd587ffd67cd9a9a51332ebac6ade638e2a327
SharonCohenOfirTrilogy <Sharon.Cohenofir@trilogy.com>
2021-05-14 17:18:02 +0300

    move to pandas and generate csv

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ import hashlib
 from itertools import count
 import urllib.parse
 import cred
+import pandas as pd
+from datetime import datetime
 
 
-def getData(url,type,nonce):
+def getData(url,nonce):
     api_key = cred.API_KEY
     api_secret_key = cred.API_SECRET_KEY
 
@@ -42,15 +44,34 @@ if __name__ == '__main__':
 
     NONCE_COUNTER = count(int(time() * 1000))
     n = next(NONCE_COUNTER)
-    print(getData(url_transactions,"orders",n))
+    data = getData(url_transactions,n)
+    df_buy  = pd.json_normalize(data, 'buyorders')
+    df_sell =pd.json_normalize(data, 'sellorders')
     n = next(NONCE_COUNTER)
-    print(getData(url_deposits,"deposits",n))
-
-
-    for b in getData(url_blnce,"balance",n).get("balances"):
+    data = getData(url_deposits,n)
+    df_deposit = pd.json_normalize(data, 'deposits')
+    n = next(NONCE_COUNTER)
+    blnc_json = []
+    for b in getData(url_blnce,n).get("balances"):
         for k in b:
-            print(f"{k},{b[k].get('balance')},{b[k].get('audbalance')},{ b[k].get('rate')}")
+            blnc_json.append({'coin':k,
+                              'balance':b[k].get('balance'),
+                              'aud-balance':b[k].get('audbalance'),
+                              'rate':b[k].get('rate')
+                              })
+    df_balance = pd.json_normalize(blnc_json)
+    df_buy["curr_rate"] = 0.0000000
+    for index,row in df_buy.iterrows():
+        coin = row['market'][0:row['market'].find("/")]
+        r = df_balance.loc[df_balance["coin"] == coin]["rate"]
+        df_buy.at[index,'curr_rate'] = r
 
-    n = next(NONCE_COUNTER)
-    x = getData(url_ord,"orders",n)
-    print(x)
+    df_buy["curr_value"]=df_buy["curr_rate"]*df_buy["amount"]
+    df_buy["profit"] = df_buy["curr_value"] / df_buy["audtotal"]
+
+    d1 = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+    df_buy[["market","amount","audtotal","curr_rate","curr_value","profit"]].sort_values(by=['profit'],ascending=False).to_csv(f"[{d1}] status.csv")
+    print("the end")
+
+
+
